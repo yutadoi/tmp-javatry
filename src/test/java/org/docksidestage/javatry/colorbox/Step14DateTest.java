b736e434ea9f0d1c190d23bdca3414b551a2871d
jflute <dbflute@gmail.com>
2019-04-24 15:19:30 +0900

    English for Step14DateTest

--- src/test/java/org/docksidestage/javatry/colorbox/Step14DateTest.java
+++ src/test/java/org/docksidestage/javatry/colorbox/Step14DateTest.java
@@ -29,24 +29,28 @@ public class Step14DateTest extends PlainTestCase {
     //                                                                               Basic
     //                                                                               =====
     /**
-     * (カラーボックスに入っている日付をスラッシュ区切りのフォーマットで表示したら？)
+     * What string is date in color-boxes formatted as slash-separated (e.g. 2019/04/24)?
+     * (カラーボックスに入っている日付をスラッシュ区切り (e.g. 2019/04/24) のフォーマットしたら？)
      */
     public void test_formatDate() {
     }
 
     /**
-     * (yellowのカラーボックスに入っているSetの中のスラッシュ区切りの日付文字列をLocalDateに変換してtoString()したら？)
+     * What string of toString() is converted to LocalDate from slash-separated date string (e.g. 2019/04/24) in Set in yellow color-box?
+     * (yellowのカラーボックスに入っているSetの中のスラッシュ区切り (e.g. 2019/04/24) の日付文字列をLocalDateに変換してtoString()したら？)
      */
     public void test_parseDate() {
     }
 
     /**
+     * What is total of month numbers of date in color-boxes?
      * (カラーボックスに入っている日付の月を全て足したら？)
      */
     public void test_sumMonth() {
     }
 
     /**
+     * What day of week is second-found date in color-boxes added to three days?
      * (カラーボックスに入っている二番目に見つかる日付に3日進めると何曜日？)
      */
     public void test_plusDays_weekOfDay() {
@@ -56,19 +60,25 @@ public class Step14DateTest extends PlainTestCase {
     //                                                                           Challenge
     //                                                                           =========
     /**
-     * (色がyellowのカラーボックスに入っている二つの日付は何日離れている？)
+     * How many days (number of day) are between two dates in yellow color-boxes?  
+     * (yellowのカラーボックスに入っている二つの日付は何日離れている？)
      */
     public void test_diffDay() {
     }
 
     /**
-     * (色がyellowのカラーボックスに入っているLocalDateに、同じカラーボックスに入っているLocalDateTimeの秒数を月数として足して、
-     * カラーボックスに入っているLong型を日数として足して、カラーボックスに入っているリストの中のBigDecimalの整数値が3の小数点第一位の数を日数として引いた日付は？)
+     * What date is LocalDate in yellow color-box
+     * that is month-added with LocalDateTime's seconds in the same color-box,
+     * and is day-added with Long value in red color-box,
+     * and is day-added with the first decimal place of BigDecimal that has three (3) as integer in List in color-boxes?
+     * (yellowのカラーボックスに入っているLocalDateに、同じカラーボックスに入っているLocalDateTimeの秒数を月数として足して、
+     * redのカラーボックスに入っているLong型を日数として足して、カラーボックスに入っているリストの中のBigDecimalの整数値が3の小数点第一位の数を日数として引いた日付は？)
      */
     public void test_birthdate() {
     }
 
     /**
+     * What second is LocalTime in color-boxes?
      * (カラーボックスに入っているLocalTimeの秒は？)
      */
     public void test_beReader() {
